0ae33d8637533e010d516d6c0893d5a524ac806f
“pavanpasupuleti09” <pavanpspk9160@gmail.com>
2026-06-25 21:40:20 +0530

    feat :basic java fundamentals practice added

---
+++ src/com/java/fundamentals/Practice.java
@@ -0,0 +1,14 @@
+package com.java.fundamentals;
+
+public class Practice {
+    public static void main(String[] args) {
+byte a = 10;
+byte b = 20;
+int c = a+b;
+
+
+        System.out.println(c);
+    }
+
+
+}
