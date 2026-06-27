15a4d0186ec2921065e8b80b11663d4734fdc7b1
“pavanpasupuleti09” <pavanpspk9160@gmail.com>
2026-06-27 12:07:03 +0530

    feat :default Constructor added

---
+++ src/com/java/fundamentals/Constructors.java
@@ -0,0 +1,12 @@
+package com.java.fundamentals;
+
+public class Constructors {
+
+        public int id;
+        public String name;
+        public boolean isAvailable;
+        public float dicountPercentage;
+
+
+    }
+
