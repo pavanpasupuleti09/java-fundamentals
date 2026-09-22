26d45ee6f5f2bcc4ccc1ee584126ee6cadf615a9
“pavanpasupuleti09” <pavanpspk9160@gmail.com>
2026-09-22 22:26:17 +0530

    feat:test

---
+++ src/com/java/fundamentals/CharTest.java
@@ -0,0 +1,16 @@
+package com.java.fundamentals;
+
+public class CharTest {
+    public static void main(String[] args) {
+
+        char ch1 = 1;
+        char ch2 = '1';
+
+        System.out.println("ch1 numeric = " + (int) ch1);
+        System.out.println("ch2 numeric = " + (int) ch2);
+
+        System.out.println("ch1 = [" + ch1 + "]");
+        System.out.println("ch2 = [" + ch2 + "]");
+    }
+
+}
