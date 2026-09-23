2cbb41fd4c0ef7d48664f374f12f3daa1aa566c0
“pavanpasupuleti09” <pavanpspk9160@gmail.com>
2026-09-24 00:00:06 +0530

    feat:primitives added

---
+++ src/com/java/fundamentals/TypeConversion.java
@@ -0,0 +1,8 @@
+package com.java.fundamentals;
+
+public class TypeConversion {
+    public static void main(String[] args) {
+        int res = 10 + 20 - 30 * 40 / 50 % 60;
+        System.out.println(res);
+    }
+}
