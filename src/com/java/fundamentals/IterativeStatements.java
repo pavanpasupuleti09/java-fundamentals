c11e858c0706d1f4c6f0bd2fd496e23f1337109f
“pavanpasupuleti09” <pavanpspk9160@gmail.com>
2026-09-25 20:58:08 +0530

    feat: add for loop to print even numbers up to N

--- src/com/java/fundamentals/IterativeStatements.java
+++ src/com/java/fundamentals/IterativeStatements.java
@@ -5,15 +5,25 @@ import java.util.Scanner;
 public class IterativeStatements {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter no : ");
+        System.out.println("Enter number : ");
         int N = scanner.nextInt();
-        int number = 1;
+        int number = 1;// loop counter
         while (number<=N){
             if (number % 2 == 0){
                 System.out.println(number);
             }
             number++;
         }
+        System.out.println("Enter number : ");
+        int N1 = scanner.nextInt();
+        for (int number1 = 1;number1 <=N1;number1++){
+            if (number1%2 ==0)
+                System.out.println(number1);
+            }
+
+        }
+
+
 
     }
-}
+
